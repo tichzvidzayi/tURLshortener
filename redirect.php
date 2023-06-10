ae363e1e79b647426e37d29875948e5c59cd3b9c
Tich Zvidzayi <tzvidzie@gmail.com>
2023-06-10 09:11:21 -0700

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,13 +1,19 @@
 <?php 
+
 require_once 'dbConfig.php';
 require_once 'Shortener.class.php';
+
 $shortener = new Shortener($db);
-$shortCode = $_GET["c"];
-try{
+$shortCode = $_GET['c'];
+
+try
+{
     $url = $shortener->shortCodeToUrl($shortCode);   //retrieve long url
-    header("Location: ".$url);
+    header("Location: " .$url);
     exit;
-}catch(Exception $e){
+}
+    catch(Exception $e)
+{
     echo $e->getMessage();
 }
 
